8ae6c814745cc77ccebdabefa5b553d86c8c074b
Joseph-A-F <149717113+Joseph-A-F@users.noreply.github.com>
2026-04-28 14:46:54 -0400

    modified:   js/visualizations/scatter.js

--- js/visualizations/scatter.js
+++ js/visualizations/scatter.js
@@ -1,1 +1,11 @@
 
+d3.json("./data/index.json").then((careerdata) => {
+  const careers_p_dept = {};
+  careerdata.forEach((d) => {
+    careers_p_dept[d.soc_group] = (careers_p_dept[d.soc_group] || 0) + 1;
+  });
+  const groups = Object.keys(soc_identifiers).map((key) => ({
+    key,
+    count: careers_p_dept[key] || 0,
+  }));
+});
