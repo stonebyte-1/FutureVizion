f0dee35bfadc7c057c8f5d953d4b8bdd47e110c7
Joseph-A-F <149717113+Joseph-A-F@users.noreply.github.com>
2026-04-28 16:23:10 -0400

    Starting Progress

--- js/visualizations/scatter.js
+++ js/visualizations/scatter.js
@@ -1,11 +1,4 @@
-
-d3.json("./data/index.json").then((careerdata) => {
-  const careers_p_dept = {};
-  careerdata.forEach((d) => {
-    careers_p_dept[d.soc_group] = (careers_p_dept[d.soc_group] || 0) + 1;
-  });
-  const groups = Object.keys(soc_identifiers).map((key) => ({
-    key,
-    count: careers_p_dept[key] || 0,
-  }));
-});
+import * as d3 from "d3";
+d3.json("./data/index.json").then((data)=>{
+    
+})
